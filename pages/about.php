08eeeec78ac7abcd954d7a26136057b081bd088e
SaraK13 <if22b237@technikum-wien.at>
2024-03-20 16:14:56 +0100

    adding about page

---
+++ pages/about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mentoring Platform</title>
+        <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/frontPage.css" rel="stylesheet">
+        <link href="../css/navbar.css" rel="stylesheet">
+        <link href="../css/about.css" rel="stylesheet">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+
+    <body>
+        <?php include('../components/header.html'); ?>
+
+        <div class="container">
+            <section class="my-5 about-container">
+                <h1>WeCanTech Mentoring</h1>
+                <p>
+                (DE) Das WeCanTech-Mentoring wird für Studentinnen aus Master-Studiengängen der FH Technikum Wien im letzten Studienjahr ausgeschrieben. 
+                Zehn ausgewählte Teilnehmerinnen erhalten ein individuelles Mentoring durch jeweils passende Mentorinnen. 
+                Die Mentorinnen sind erfolgreiche Technikerinnen von WeCanTech-Firmenpartnerschaften. 
+                Dabei entsteht eine persönlich gestaltete Beziehung zwischen einer beruflich erfahrenen Frau und einer Studentin. 
+                Ziel ist es, Karrieremöglichkeiten aufzuzeigen, Branchen-Knowhow zu vermitteln, Erfahrungen auszutauschen sowie einen persönlichen Kontakt zum Unternehmen zu schaffen. 
+                Mentoring ist ein besonders starkes Instrument zur Förderung von Frauen!
+                <br>Detaillierte Informationen sind hier:
+                </p>
+                <p class="about-paragraph">(EN) The WeCanTech mentoring is advertised for female students from Master's programs 
+                    in their final year of study. Ten selected participants receive individual mentoring from suitable mentors. 
+                    This creates a personalized relationship between a woman with professional experience and a female student. 
+                    The aim is to highlight career opportunities, impart industry know-how, exchange experiences and establish personal contact with the company. 
+                    <br>Mentoring is a particularly powerful tool for promoting women!</p>
+                <p>Detailed Information:</p>
+            
+                <div class="document-link">
+                    <a href="path_to_detailed_information_2023" class="pdf-link">
+                        <i class="bi bi-file-earmark-pdf"></i>
+                        WeCanTechMentoring Ausschreibung 2023
+                    </a>
+                </div>
+                <div class="document-link">
+                    <a href="path_to_mentoring_call_2023" class="pdf-link">
+                    <i class="bi bi-file-earmark-pdf"></i>
+                    WeCanTech Mentoring Call 2023
+                    </a>
+                </div>
+            </section>
+        </div>
+        <footer>
+            <?php include('../components/footer.html'); ?>
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+</html>
